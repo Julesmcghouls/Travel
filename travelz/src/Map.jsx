83eb84f48316a85386ad522090750e9e019c9de9
Julian <jjvll90@gmail.com>
2024-07-02 23:30:12 -0500

    Initial commit - Set up Map

---
+++ travelz/src/Map.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef } from "react";
+import tt from "@tomtom-international/web-sdk-maps";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
+
+const Map = () => {
+
+}
+
+export default Map
