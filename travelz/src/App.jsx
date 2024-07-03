29d7551e8a3e527f1c90f85f125000ce4db4d82f
Julian <jjvll90@gmail.com>
2024-07-02 23:45:53 -0500

    Added Map component to App

--- travelz/src/App.jsx
+++ travelz/src/App.jsx
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import Map from './Map'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <Map />
     </>
   )
 }
