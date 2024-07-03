7c48e2b0c5124536afb31a9e7122902202f3a0c7
Julian <jjvll90@gmail.com>
2024-07-02 23:44:11 -0500

    Add TomTom map component with navigation

--- travelz/src/Map.jsx
+++ travelz/src/Map.jsx
@@ -3,7 +3,20 @@ import tt from "@tomtom-international/web-sdk-maps";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
 const Map = () => {
+    const mapElement = useRef(null);
 
-}
+    useEffect(() => {
+        const map = tt.map({
+            key: "b805ed21-a42c-418b-8cbf-da915bf679f7",
+            container: mapElement.current,
+            style: "tomtom://vector/1/basic-main", 
+        });
+        map.addControl(new tt.NavigationControl());
 
-export default Map
+    return () => map.remove();
+}, []);
+
+return <div id="map" ref={mapElement} style={{ height: '100vh', width: '100vw' }}></div>;
+};
+
+export default Map;
